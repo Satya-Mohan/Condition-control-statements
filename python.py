b2a2c6d62c402d171641062075ca5fdf4ebf9d42
Satya-Mohan <112909110+Satya-Mohan@users.noreply.github.com>
2022-09-06 11:16:35 -0500

    Add files via upload

---
+++ python.py
@@ -0,0 +1,13 @@
+print("Exam Results")
+Marks = 36
+if Marks == 35:
+    print("You got pass Marks")
+elif Marks >= 35:
+    print("you got good Marks")
+elif Marks >= 70:
+    print("you got best Marks")
+else:
+    print("you got fail Marks")
+
+
+  
